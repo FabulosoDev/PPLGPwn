d1f77554ef5a96600deccc1c7f4f7599f62691bd
FabulosoDev <113933725+FabulosoDev@users.noreply.github.com>
2024-05-14 00:43:17 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,82 @@
+clear 
+
+GREEN='\033[0;92m' 
+BLUE='\033[0;94m'
+PURPLE='\033[0;95m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}
+
+
+8888888b.  8888888b.  888      .d8888b.  8888888b.  888       888 888b    888 
+888   Y88b 888   Y88b 888     d88P  Y88b 888   Y88b 888   o   888 8888b   888 
+888    888 888    888 888     888    888 888    888 888  d8b  888 88888b  888 
+888   d88P 888   d88P 888     888        888   d88P 888 d888b 888 888Y88b 888 
+8888888P   8888888P   888     888  88888 8888888P   888d88888b888 888 Y88b888 
+888        888        888     888    888 888        88888P Y88888 888  Y88888 
+888        888        888     Y88b  d88P 888        8888P   Y8888 888   Y8888 
+888        888        88888888 Y8888P88  888        888P     Y888 888    Y888 
+
+${NC}
+Designed for LG webOS TVs! Ported by ${PURPLE}Kodeine${NC}, with luv <3
+With the contribution of ${BLUE}llbranco${NC}!
+Version: v1.1
+${CYAN}-------
+INSTALLATION
+-------${NC}
+"
+echo -e "
+
+${CYAN}-------
+Downloading files...
+-------${NC}
+"
+wget https://github.com/zauceee/PPLGPwn/archive/refs/heads/zauceee-PPLGPwn-v-1-1.zip
+
+echo -e "Done!
+
+${CYAN}-------
+Setting up files...
+-------${NC}
+"
+
+unzip PPLGPwn-main.zip
+
+mv ./PPLGPwn-main/PPLGPwn-main /media/internal/downloads/PPLGPwn
+
+echo -e "Done!
+
+${CYAN}-------
+CONFIGURATION
+-------${NC}
+"
+
+read -p "What's the interface's name? " int
+read -p "Firmware version (750-1100): " fmv
+
+echo "Writing settings..."
+echo "
+interface=$int 
+firmver=$fmv
+stage1=/media/internal/downloads/PPLGPwn/stage1.bin
+stage2=/media/internal/downloads/PPLGPwn/stage2.bin
+" > /media/internal/downloads/PPLGPwn/settings.config
+echo "Done!
+"
+echo "If you wish to change the stage2.bin go into /media/internal/downloads/PPLGPwn and replace the exisiting stage2.bin!
+"
+
+echo "To run the exploit execute "run.sh" present in the mentioned directory! But to make it simplier follow the steps to execute the exploit with the click of a button! :)"
+echo "Enjoy ;)"
+luna-send -a webosbrew -f -n 1 luna://com.webos.notification/createToast '{"sourceId":"webosbrew","message": "<b>PPLGPwn installed.</b>"}'
+
+echo "Running PPLGPWN in 3 seconds..."
+sleep 3
+
+cd /media/internal/downloads/PPLGPwn
+chmod +x ./run.sh
+./run.sh
+
+
+
+
