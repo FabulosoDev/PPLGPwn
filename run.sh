ba86ec7f9d40b622af1e94db65cf648fd39215e5
FabulosoDev <113933725+FabulosoDev@users.noreply.github.com>
2024-05-16 08:21:21 +0200

    - update run.sh

--- run.sh
+++ run.sh
@@ -31,5 +31,5 @@ luna-send -a webosbrew -f -n 1 luna://com.webos.notification/createToast '{"sour
 
 chmod +x ./pppwn
 ./pppwn --interface $interface --fw $firmver --stage1 $stage1 --stage2 $stage2 --auto-retry
-echo "./pppwn --interface $interface -fw $firmver --stage1 $stage1 --stage2 $stage2"
+echo "./pppwn --interface $interface -fw $firmver --stage1 $stage1 --stage2 $stage2 --auto-retry"
 luna-send -a webosbrew -f -n 1 luna://com.webos.notification/createToast '{"sourceId":"webosbrew","message": "<b>PPLGPwned!</b><br/>Done, enjoy! ;)"}'
